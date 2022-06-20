6ad0168d7b6008c03bd6079127fc766340a096da
kevit <rdsankhavara2207@gmail.com>
2022-06-20 19:08:15 +0530

    Testing Create User

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+require("./db/mongoose")
+const User = require("./models/user");
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+// It allow us to use json data from the req body
+app.use(express.json());
+
 app.post("/users", (req, res) => {
-    res.send("User Testing");
+    res.send(req.body);
+    console.log(req.body);
 })
 
 app.listen(port, () => {
---
+++ src/models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const User = mongoose.model("User", {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        // Write validation code in this function
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Please enter valid email");
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            if (value.toLowerCase().includes("password")) {
+                throw new Error("Can't use the word password in your password");
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0
+    }
+})
+
+module.exports = User;
